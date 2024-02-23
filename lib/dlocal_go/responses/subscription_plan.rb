e2c196b03ff6c29d328e72e7d6fe5c158439989d
Matias Salles <matiassalles99@gmail.com>
2024-02-23 18:38:46 -0300

    fix

--- lib/dlocal_go/responses/subscription_plan.rb
+++ lib/dlocal_go/responses/subscription_plan.rb
@@ -5,7 +5,7 @@ module DlocalGo
     # Class that represents Dlocal Go subscription plan schema
     class SubscriptionPlan
       RESPONSE_ATTRIBUTES = %i[
-        id merchand_id name description country currency amount frequency_type frequency_value active free_trial_days
+        id merchant_id name description country currency amount frequency_type frequency_value active free_trial_days
         plan_token back_url notification_url success_url error_url created_at updated_at
       ].freeze
 
